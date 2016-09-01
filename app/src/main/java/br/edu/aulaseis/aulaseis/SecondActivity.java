43ef0cf710bdacc497b84cbbf02c9287c7b64bed
welder.souza <welder.souza@cnj.jus.br>
2016-09-01 16:48:05 -0300

    Adicao de Tela Dois

---
+++ app/src/main/java/br/edu/aulaseis/aulaseis/SecondActivity.java
@@ -0,0 +1,16 @@
+package br.edu.aulaseis.aulaseis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by welder on 01/09/16.
+ */
+public class SecondActivity extends Activity{
+
+    public void onCreate(Bundle stateActivity){
+        super.onCreate(stateActivity);
+        setContentView(R.layout.activity_second);
+    }
+
+}
