b035fea2ad4de220e08bbb2936b5f61ff322bb60
welder.souza <welder.souza@cnj.jus.br>
2016-09-01 17:02:31 -0300

    Volta para a tela principal

--- app/src/main/java/br/edu/aulaseis/aulaseis/MainActivity.java
+++ app/src/main/java/br/edu/aulaseis/aulaseis/MainActivity.java
@@ -1,13 +1,28 @@
 package br.edu.aulaseis.aulaseis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button chamar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chamar = (Button) findViewById(R.id.button);
+
+        chamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/edu/aulaseis/aulaseis/SecondActivity.java
+++ app/src/main/java/br/edu/aulaseis/aulaseis/SecondActivity.java
@@ -1,16 +1,32 @@
 package br.edu.aulaseis.aulaseis;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by welder on 01/09/16.
  */
 public class SecondActivity extends Activity{
 
-    public void onCreate(Bundle stateActivity){
+    private Button voltar;
+
+    public void onCreate(final Bundle stateActivity){
         super.onCreate(stateActivity);
         setContentView(R.layout.activity_second);
+        voltar = (Button) findViewById(R.id.buttonVoltar);
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
 }
